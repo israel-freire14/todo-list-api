1fcbe76aa19c87b4dd6b73459c38336b8ac6d62c
Israel Freire <israelfreire999@gmail.com>
2024-07-25 17:59:30 -0300

    feat: config build project

---
+++ src/main/java/com/tech/todolistapi/TodolistapiApplication.java
@@ -0,0 +1,11 @@
+package com.tech.todolistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistapiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistapiApplication.class, args);
+	}
+}
