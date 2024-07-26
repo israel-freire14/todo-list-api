d107f7b8ecadaed385833a562f75c9a595697731
Israel Freire <israelfreire999@gmail.com>
2024-07-26 19:38:19 -0300

    fix: fixing indentation

--- src/main/java/com/tech/todolistapi/services/TaskService.java
+++ src/main/java/com/tech/todolistapi/services/TaskService.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
