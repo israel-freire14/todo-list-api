a194dbeeae17bc69e5dab98324c440f6895826db
Israel Freire <israelfreire999@gmail.com>
2024-08-02 18:10:14 -0300

    fix: fixing print task object at log

--- src/main/java/com/tech/todolistapi/controllers/TaskController.java
+++ src/main/java/com/tech/todolistapi/controllers/TaskController.java
@@ -1,9 +1,12 @@
 package com.tech.todolistapi.controllers;
 
+import com.google.gson.Gson;
 import com.tech.todolistapi.entities.Task;
 import com.tech.todolistapi.services.TaskService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,15 +18,15 @@ import java.util.List;
 @AllArgsConstructor
 @Slf4j
 public class TaskController {
-
-
+    private static final Logger logger =  LogManager.getLogger(TaskController.class.toString());
+    private  static final Gson GSON = new Gson();
 
     private TaskService taskService;
 
     @PostMapping("/tasks")
     @ResponseStatus(HttpStatus.CREATED)
     public Task create(@RequestBody Task task){
-        log.info("Creating a new task with info [{}]", task.toString());
+        logger.info("Creating a new task with info [{}]", GSON.toJson(task));
         return  taskService.createTask(task);
     }
 
@@ -45,7 +48,7 @@ public class TaskController {
     @PutMapping("/tasks/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Task> updateTaskById(@PathVariable(value = "id") Long id, @RequestBody Task task) {
-        log.info("Updating task by id [{}] and the new info are: [{}]", id, task);
+        log.info("Updating task by id [{}] and the new info are: [{}]", id, GSON.toJson(task));
         return taskService.updateTaskById(task,id);
     }
 
